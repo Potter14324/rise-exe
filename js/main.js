93d6c40c4e46144fa4e85daece6ff06c1ca56dfb
Potter14324 <132168110+Potter14324@users.noreply.github.com>
2024-11-01 17:54:06 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5910,11 +5910,11 @@ document.head.appendChild(style);
                     }, [s("span", [e._v("UI Themes")]), e._v(" "), s("color-option", {
                         staticClass: "right",
                         attrs: {
-                            value: e.fadeBoxBackgroundColor
+                            value: e.fadeBoxBackground
                         },
                         on: {
                             input: function(t) {
-                                return e.change("fadeBoxBackgroundColor", t)
+                                return e.change("fadeBoxBackground", t)
                             }
                         }
                     })], 
