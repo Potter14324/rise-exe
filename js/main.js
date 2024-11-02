5f2b2bec5b47bed383174e476a39fc4b4ee2d6b0
Potter14324 <132168110+Potter14324@users.noreply.github.com>
2024-11-02 15:08:54 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5901,23 +5901,30 @@ document.head.appendChild(style);
                                           
 
 
+        
+                                s("div", {
+            staticClass: "color-input"
+        }, [
+            s("span", [e._v("UI Themes")]),
+            e._v(" "),
+            s("color-option", {
+                staticClass: "right",
+                attrs: {
+                    value: e.fadeBoxBackground
+                },
+                on: {
+                    input: function(t) {
+                        e.change("fadeBoxBackground", t);
+                        document.querySelector('.fade-box').style.background = `linear-gradient(to bottom, ${t}, #202020)`;
+                    }
+                }
+            })
+        ])
 
-                        
 
                                           
-                        1), e._v(" "), s("div", {
-                        staticClass: "color-input"
-                    }, [s("span", [e._v("UI Themes")]), e._v(" "), s("color-option", {
-                        staticClass: "right",
-                        attrs: {
-                            value: e.fadeBoxBackground
-                        },
-                        on: {
-                            input: function(t) {
-                                return e.change("fadeBoxBackground", t)
-                            }
-                        }
-                    })], 
+                       
+                   
 
 
 
