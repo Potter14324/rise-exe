2144165cf9332d0dfe91d2ce79900fff94cc0196
Potter14324 <132168110+Potter14324@users.noreply.github.com>
2024-11-08 20:41:08 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5902,11 +5902,11 @@ document.head.appendChild(style);
                     }, [s("span", [e._v("Name outline")]), e._v(" "), s("color-option", {
                         staticClass: "right",
                         attrs: {
-                            value: e.fadeBoxBackground
+                            value: e.fadeBoxColor
                         },
                         on: {
                             input: function(t) {
-                                return e.change("fadeBoxBackground", t)
+                                return e.change("fadeBoxColor", t)
                             }
                         }
                     })],    
